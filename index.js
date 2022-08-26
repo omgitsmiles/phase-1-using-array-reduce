f98f93fe85577927e816637162500afbdbb91d46
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-08-26 13:37:30 -0400

    commit tree 2a42ab60bbc753711e4b197203fbcd05d84cdc24

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// const totalBatteries = batteryBatches.reduce((acc, int) => acc + int, 0)
+
+//in batterybatches array, use reduce method to get sum of all batterys into single value
+const totalBatteries = batteryBatches.reduce((acc, currVal) => acc + currVal)
